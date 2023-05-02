c813232780a84fc58dd4490417ff69b19add6fbb
unknown <mido1elsafty@gmail.com>
2023-05-02 04:22:15 +0300

    Add your commit

---
+++ training.js
@@ -0,0 +1,19 @@
+for (let i=0 ; i<=10 ; i++){
+    console.log(i);
+}
+
+<script>
+    
+var udaciFamily=["julia","james","mohammed"];
+    
+    for(var index =0 ;index <udaciFamily.lenth ;index ++){
+    console.log(udaciFamily[index]);    
+    }
+    
+</script>
+
+var udaciFamily=["julia","james","mohammed"];
+    
+for(var index =0 ;index <udaciFamily.lenth ;index ++){
+console.log(udaciFamily[index]);    
+}
